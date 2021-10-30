2a43f31ba2ee453d66e58ae2a337331b6c26734c
Richard Wang <richardwang981@yahoo.com>
2021-10-30 17:17:21 -0500

    Fixing merge conflict

---
+++ ConsiliaAPI/Objects/Places.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsiliaAPI.Objects
+{
+    public class Places
+    {
+        public Guid PlaceID { get; private set; }
+
+        public Guid EventID { get; private set; }
+
+        public static async Task<Places> PlacesRequest()
+        {
+            string request = "https://maps.googleapis.com/maps/api/place/findplacefromtext/output?parameters";
+"; 
+        }
+    }
+
+
+}
---
+++ ConsiliaAPI/Objects/Vote.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace ConsiliaAPI.Objects
+{
+    public class Vote
+    {
+        public Guid PlaceUUID { get; private set; }
+
+        public Guid EventUUID { get; private set; }
+
+        public Guid UserUUID { get; private set; }
+
+        public Guid VoteUUID { get; private set; }
+
+        public int VoteType { get;  set; }
+    }
+}
