afeeb916cd5073ce5aa49aaab9fae500c8da66cb
Richard Wang <richardwang981@yahoo.com>
2021-10-30 16:07:26 -0500

    Create event function implementation

--- ConsiliaAPI/Objects/Event.cs
+++ ConsiliaAPI/Objects/Event.cs
@@ -4,12 +4,50 @@ namespace ConsiliaAPI.Objects
 {
     public class Event
     {
-        public string EventID { get; private set; }
+        public Guid EventID { get; private set; }
         public string Name { get; set; }
         public DateTime StartDate { get; set; }
         public double LocationLat { get; set; }
         public double LocationLong { get; set; }
         public double Range { get; set; }
         public string Type { get; set; }
+
+        public static async Task<User> CreateEvent(string name, DateTime date, double lat, double longitude, double range, double type)
+        {
+            try
+            {
+                Event e = new Event();
+                // Generate User's details
+                e.EventID = Guid.NewGuid();
+                e.Name = name;
+                e.StartDate = date;
+                e.LocationLat = lat;
+                e.LocationLong = longitude;
+                e.Range = range;
+                e.Type = type;
+
+                // Insert them into database
+                NpgsqlConnection conn = Database.DatabaseConnection;
+                NpgsqlCommand command =
+                    new NpgsqlCommand(
+                        $"INSERT INTO EVENTS(event_uuid, event_name, start_date_time, latitude, longitude, range, type) " +
+                        $"VALUES(\'{e.EventID}\', \'{e.Name}\', \'{e.StartDate}\', \'{e.LocationLat}\'), \'{e.LocationLong}\'), \'{e.Range}\', \'{e.Type}\'))", conn);
+                await command.ExecuteNonQueryAsync();
+
+                return e;
+            }
+            catch (Exception e)
+            {
+                throw new ConvoyException("Unable to create event.", HttpStatusCode.InternalServerError, e.StackTrace);
+            }
+        }
+
+        public static async Task CastVote(bool vote, Guid user)
+        {
+
+        }
+
     }
+
+
 }
